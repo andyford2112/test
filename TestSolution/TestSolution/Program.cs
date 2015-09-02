892969ee31bfd0f89e6a2668019ef797a28f5ad5
andyford <andyford1127@gmail.com>
2015-09-02 15:27:04 -0600

    Added Read

--- TestSolution/TestSolution/Program.cs
+++ TestSolution/TestSolution/Program.cs
@@ -18,6 +18,7 @@ namespace TestSolution
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             Console.WriteLine("Hey");
+            Console.Read();
         }
     }
 }
